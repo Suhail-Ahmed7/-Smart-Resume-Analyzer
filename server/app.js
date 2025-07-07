c517de8c05c597ef3bf516377d780c7fec114b0a
Suhail-Ahmed7 <suhailbhand7@gmail.com>
2025-07-07 16:02:51 +0500

    App Updated.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 require('dotenv').config()
+
+const authRoutes = require('./routes/authRoutes');
 require('./config/dbConnection')()
 const app = express()
 
 
 app.use(express.json())
+app.use('/api/auth', authRoutes);
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server running on port, ${PORT}`,);
